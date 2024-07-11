9263a6d320fdba5a0f8d0308ae9f967cad8a1e78
Ananev Nikita <ananev.nikita.002@gmail.com>
2024-07-11 22:36:57 +0500

    lint(test/main): format test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('empty', () {});  // иначе very_good test --recursive возвращает 69
+  test('empty', () {}); // иначе very_good test --recursive возвращает 69
 }
