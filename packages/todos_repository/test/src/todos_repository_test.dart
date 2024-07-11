f4870f994eba4beec31fa4da9122f9a2cf1b68ad
Ananev Nikita <ananev.nikita.002@gmail.com>
2024-07-11 22:28:42 +0500

    test,packages(app): add test for each package, update deps

--- packages/todos_repository/test/src/todos_repository_test.dart
+++ packages/todos_repository/test/src/todos_repository_test.dart
@@ -1,2 +1,28 @@
 // ignore_for_file: prefer_const_constructors
-void main() {}
+import 'package:test/test.dart';
+import 'package:todos_api/todos_api.dart';
+import 'package:todos_repository/src/multiple_todos_repository.dart';
+import 'package:todos_repository/src/single_todos_repository.dart';
+
+class MockTodosApi extends TodosApi {
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+void main() {
+  group('MultipleTodosRepository', () {
+    test('Can be init', () {
+      MultipleTodosRepository(
+        todosApis: [
+          MockTodosApi(),
+          MockTodosApi(),
+        ],
+      );
+    });
+  });
+  group('SingleTodosRepository', () {
+    test('Can be init', () {
+      SingleTodosRepository(todosApi: MockTodosApi());
+    });
+  });
+}
